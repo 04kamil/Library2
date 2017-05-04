68e3c574a662427c4e78d3e22a20267958d18dee
Kamil Brozek <04kamil@gmail.com>
2017-05-04 23:38:33 +0200

    Dodanie ConnectionStringa i poczatek pisania DAL

---
+++ Library2/DAL/LibraryContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Library2.DAL
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(): base("LibraryContext")
+        {
+
+        }
+
+    }
+}

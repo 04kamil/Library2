76fd34c8aa69ed1637784adea0c5e8c8695caa0a
Kamil Brozek <04kamil@gmail.com>
2017-05-04 23:47:02 +0200

    Dodanie modelu Autor

---
+++ Library2/Models/Autor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Library2.Models
+{
+    public class Autor
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid AuthorID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Life { get; set; }
+        public string Descryption { get; set; }
+
+    }
+}
